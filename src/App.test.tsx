14762ad7f8ee3feb39801867b078b61c4dee133f
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-03-09 00:08:09 -0500

    Clean out CRA boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders something with "ui"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/ui/i);
+  expect(element).toBeInTheDocument();
 });
